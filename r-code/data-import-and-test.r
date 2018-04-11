531882d1638e9ac11b02de1352dbd9f8a8a40dfb
Jinwoo Chung <chungjinwoo5d@naver.com>
2018-04-11 10:38:12 +0900

    update
    update

--- r-code/data-import-and-test.r
+++ r-code/data-import-and-test.r
@@ -1,8 +1,8 @@
 library(tidyverse)
 
-df.2015 <- read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2015.csv')
-df.2016 <- read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2016.csv')
-df.2017 <- read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2017.csv')
+df.2015 <- readr::read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2015.csv')
+df.2016 <- readr::read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2016.csv')
+df.2017 <- readr::read_csv(file='~/Documents/GitHub/fcdc-overflow-project-1/data-raw/2017.csv')
 
 # initializing global variables
 reset.var <- c('Country','Region','HappinessRank','HappinessScore','StandardError','EconomyGDP','Family','LifeExpectancy'
@@ -10,8 +10,12 @@ reset.var <- c('Country','Region','HappinessRank','HappinessScore','StandardErro
 dependent.index <- 4
 independent.index <- 6:12
 
-lm.test <- basic_linear_reg_test(df.2015,reset.var,dependent.index,independent.index)
-summary(lm.test)
+lm.2015test <- basic_linear_reg_test(df.2015,reset.var,dependent.index,independent.index)
+lm.2016test <- basic_linear_reg_test(df.2016,reset.var,dependent.index,independent.index)
+lm.2017test <- basic_linear_reg_test(df.2017,reset.var,dependent.index,independent.index)
+summary(lm.2015test)
+summary(lm.2016test)
+summary(lm.2017test)
 
 basic_linear_reg_test <- function(df,set.colname,dep.colindex,indep.colindex) {
   colnames(df) <- set.colname
